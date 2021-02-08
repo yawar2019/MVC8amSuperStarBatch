9ccdb78c3a22a2814fdb543d413b5a0c38f5e35f
yawar2019 <yawarali17@gmail.com>
2021-02-08 09:09:12 +0530

    committed Code

---
+++ AdoNetExample/Models/EmployeeContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoNetExample.Models
+{
+    public class EmployeeContext
+    {
+        public List<EmployeeModel> GetEmployee() {
+
+
+            List<EmployeeModel> listemp = new List<EmployeeModel>();
+            SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true;");
+            SqlCommand cmd = new SqlCommand("sp_getRituEmployees",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)//2 ritu and vinod
+            {
+                EmployeeModel emp = new EmployeeModel();
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+                listemp.Add(emp);
+
+            }
+            return listemp;
+        }
+    }
+}
